fbe8c219cbead5c281c4a961d6121f5af90175d8
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2025-02-20 16:42:14 +0100

    create customer class

---
+++ Snackautomat Challenge/src/ch/noseryoung/blj/Customer.java
@@ -0,0 +1,16 @@
+package ch.noseryoung.blj;
+
+public class Customer {
+    private double credit;
+
+    public Customer(){
+        credit = 100;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+    public void setCredit(double newCredit){
+        credit = newCredit;
+    }
+}

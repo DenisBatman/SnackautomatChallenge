e0f8b426d0a160adbef1eafb8b4f387cc209fca1
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2025-02-20 15:58:19 +0100

    create product constructor

--- Snackautomat Challenge/src/ch/noseryoung/blj/Sort.java
+++ Snackautomat Challenge/src/ch/noseryoung/blj/Sort.java
@@ -1,4 +1,13 @@
 package ch.noseryoung.blj;
 
 public class Sort {
+    private final int price;
+
+    public Sort(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }

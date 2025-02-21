bd42eadb5826e92704070cf1ef68f8dc951b4565
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2025-02-21 08:20:41 +0100

    fill vending machine at start and after product shortage

--- Snackautomat Challenge/src/ch/noseryoung/blj/VendingMachine.java
+++ Snackautomat Challenge/src/ch/noseryoung/blj/VendingMachine.java
@@ -1,4 +1,46 @@
 package ch.noseryoung.blj;
 
+import java.util.ArrayList;
+
 public class VendingMachine {
+    private ArrayList<Product> products;
+    private final int maxAmountOfProducts = 10;
+
+    public VendingMachine() {
+        products = new ArrayList<>();
+    }
+
+    public void fillEmptyMachine(){
+        for(int i = 0; maxAmountOfProducts > i; i++){
+            products.add(new Product(Type.COLA));
+            products.add(new Product(Type.SPRITE));
+            products.add(new Product(Type.SNICKERS));
+            products.add(new Product(Type.SANDWICH));
+        }
+    }
+    public void buyProduct(Type type){
+        for (Product product : products){
+            if(product.getProductSort() == type){
+                products.remove(product);
+                break;
+            }
+        }
+        fillProductStock(type);
+    }
+
+    public void fillProductStock(Type type) {
+        int amountOfProductOfThisSort = 0;
+        for (Product product : products){
+            if(product.getProductSort() == type){
+                amountOfProductOfThisSort++;
+                break;
+            }
+        }
+        if(amountOfProductOfThisSort < maxAmountOfProducts * 0.3){
+            for (int i = amountOfProductOfThisSort; maxAmountOfProducts > amountOfProductOfThisSort;
+                 amountOfProductOfThisSort++){
+                products.add(new Product(type));
+            }
+        }
+    }
 }

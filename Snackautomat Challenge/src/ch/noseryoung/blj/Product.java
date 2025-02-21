62e3b0415bc89ebe9e84bb0f1a5f8c92d89c939b
DenisBatman <denis.suciu@edu.tbz.ch>
2025-02-21 13:21:04 +0100

    Revert "Merge pull request #13 from DenisBatman/revert-12-Fetaure/add-new-product-sorts"
    This reverts commit 91e7c4cb9b087fe8c4cea5803bd67b2e01b22278, reversing
    changes made to 566feba7aed923c9e8960a0ac90c66bc25e57e3e.

--- Snackautomat Challenge/src/ch/noseryoung/blj/Product.java
+++ Snackautomat Challenge/src/ch/noseryoung/blj/Product.java
@@ -4,29 +4,19 @@ import java.util.UUID;
 
 public class Product {
     private final UUID productId;
-    private final Type productSort;
-    private final double price;
+    private final String productSort;
 
-    public Product(Type productSort) {
+    public Product(String productSort) {
         this.productSort = productSort;
         productId = UUID.randomUUID();
-        switch(productSort) {
-            case COLA -> price = 1.5;
-            case SPRITE -> price = 1.6;
-            case SNICKERS -> price = 2.5;
-            default -> price = 7.5;
-        }
     }
-
-
 
     public UUID getProductId() {
         return productId;
     }
 
-    public Type getProductSort() {
+    public String getProductSort() {
         return productSort;
     }
 
-    public double getPrice() {return price;}
 }

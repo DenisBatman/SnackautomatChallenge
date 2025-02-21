e68df0147f0706140f0f19b54e569948db4033b6
DenisBatman <denis.suciu@edu.tbz.ch>
2025-02-21 09:50:19 +0100

    Control If Customer is able to buy Product
    -

--- Snackautomat Challenge/src/ch/noseryoung/blj/Main.java
+++ Snackautomat Challenge/src/ch/noseryoung/blj/Main.java
@@ -1,6 +1,12 @@
 package ch.noseryoung.blj;
+
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-}
+    }
+
+

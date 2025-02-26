f6abaa71b165a74cb1080069c086b96de2251343
Michel Mahadeva <michel.mahadeva@edu.tbz.ch>
2025-02-26 08:52:14 +0100

    begin transaction and ui

--- Snackautomat Challenge/src/ch/noseryoung/blj/Main.java
+++ Snackautomat Challenge/src/ch/noseryoung/blj/Main.java
@@ -1,11 +1,22 @@
 package ch.noseryoung.blj;
 
 
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        JFrame window = new JFrame("Vending Machine");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
 
+        UI userInterface = new UI();
+        window.add(userInterface);
+        window.pack();
 
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        userInterface.launchGame();
     }
 }
 
